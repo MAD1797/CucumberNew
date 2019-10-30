b4cea9673731e4a46902f2e7963e91b15c8831b5
A07216trng_M7A.05.19 <A07216trng_M7A.05.19@M7-DX-79JFNK2.dir.svc.accenture.com>
2019-10-30 17:27:27 +0530

    hello

---
+++ CucumberNew/src/test/java/testRunner/LoginTestRunner.java
@@ -0,0 +1,22 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+		features="Feature//Login.feature",
+		glue= {"stepDefinition"},
+		monochrome=true,
+		plugin={"pretty","html:test-output"}
+				
+		)
+
+
+public class LoginTestRunner {
+
+}
